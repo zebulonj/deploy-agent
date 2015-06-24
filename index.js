e14ca37040cf5962608d5393f11edf6d3e2efe8f
Zebulon Young <zebulonj@gmail.com>
2015-06-24 08:36:09 -0700

    Sanity check using expresses (#2).

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 'use strict';
 
-import ServerFactory from 'gith';
+import express from 'express';
+import BodyParser from 'body-parser';
 
-const agent = ServerFactory.create( 9001 );
+const app = express();
 
-agent()
-	.on( 'all', function( payload ) {
-		console.log( "Push:", payload );
-	});
+// Middleware
+app.use( BodyParser.json() );
+
+// Log all requests and return 200 status.
+app.use( function( req, res ) {
+	console.log( "Request:", req.method, req.url, req.body );
+
+	res.status( 200 ).end();
+});
+
+app.listen( 9001 );
 
 console.log( "Listening..." );
